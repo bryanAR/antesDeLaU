9ee4a2a367866b85355d6eacba79d4d945f45171
bryanRA <bryanroa26@gmail.com>
2021-12-31 23:31:19 -0500

    agregando entradas de teclado, ya casi es año nuevo, chao :v

---
+++ lista_de_compras.py
@@ -0,0 +1,8 @@
+
+la_hora_actual=int(input("¿Que hora es? "))
+la_hora_de_cierre=10
+
+if la_hora_actual > la_hora_de_cierre:
+    print("no hay hamburguesas, tenemos hambre")
+else:
+    print("si hay hamburguesas, podemos comer")
